ed4356fa836e70bdf3459a074a136da286f99ed0
Christian Sales <christiansales07@outlook.com>
2026-06-25 23:48:16 -0300

    boa

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Humanamente-MVP/', // Set the base path for your app
+  base: '/Humanamente-MVP/',
 });
